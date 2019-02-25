81de09a31badd69b0be00e53ea1418f32e0b86df
Adam Jean-Laurent <44792003+adamjeanlaurent@users.noreply.github.com>
2019-02-25 17:33:31 -0500

    Refactored index.js

--- index.js
+++ index.js
@@ -1,47 +1,10 @@
 var playerOne = Math.floor(Math.random() * 6) + 1;
 var playerTwo = Math.floor(Math.random() * 6) + 1;
 
-switch(playerOne){
-    case 1: 
-        document.querySelector(".img1").setAttribute("src", "images/dice1.png");
-        break;
-    case 2:
-        document.querySelector(".img1").setAttribute("src", "images/dice2.png");
-        break;
-    case 3:
-        document.querySelector(".img1").setAttribute("src", "images/dice3.png");
-        break;
-    case 4:
-        document.querySelector(".img1").setAttribute("src", "images/dice4.png");
-        break;
-    case 5:
-        document.querySelector(".img1").setAttribute("src", "images/dice5.png");
-        break;
-    case 6:
-        document.querySelector(".img1").setAttribute("src", "images/dice6.png");
-        break;
-}
 
-switch(playerTwo){
-    case 1: 
-        document.querySelector(".img2").setAttribute("src", "images/dice1.png");
-        break;
-    case 2:
-        document.querySelector(".img2").setAttribute("src", "images/dice2.png");
-        break;
-    case 3:
-        document.querySelector(".img2").setAttribute("src", "images/dice3.png");
-        break;
-    case 4:
-        document.querySelector(".img2").setAttribute("src", "images/dice4.png");
-        break;
-    case 5:
-        document.querySelector(".img2").setAttribute("src", "images/dice5.png");
-        break;
-    case 6:
-        document.querySelector(".img2").setAttribute("src", "images/dice6.png");
-        break;
-}
+document.querySelector(".img1").setAttribute("src", "images/dice" + playerOne + ".png");
+
+document.querySelector(".img2").setAttribute("src", "images/dice" + playerTwo + ".png");
 
 
 if(playerOne > playerTwo){
